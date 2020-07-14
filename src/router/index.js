e45024dc8fba19ae5f023b1aa0478d3cca9c99dd
py-ztw <1513185373@qq.com>
2020-07-14 11:21:28 +0800

    课程页前端day5

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Home from "../components/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import Course from "../components/Course";
+import CourseDetail from "../components/CourseDetail";
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +13,8 @@ export default new Router({
            {path: '/home/', component: Home,},
            {path: '/home/login/', component: Login,},
            {path: '/user/register', component: Register,},
+           {path: '/python', component:Course},
+           {path: '/course/detail', component: CourseDetail,},
 
   ]
 })
